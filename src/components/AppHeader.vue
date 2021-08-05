bd598ed139a400792c5daae0e9271d446cc24383
Benjamin Akhimien <77724650+Houzer21@users.noreply.github.com>
2021-08-05 20:49:57 +0100

    Add files via upload

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <div class="header__logo">
+      888<span>gc</span>
+    </div>
+    <nav class="header__nav">
+      <input type="checkbox" id="menu" class="hamburger__toggler" ref="menu" />
+      <!---ul class="nav">
+        <li class="nav__list">Leadership Board</li>
+        <li class="nav__list">Teams</li>
+      </ul --->
+      <router-link to="/login" class="header__link">
+        <i class="lni lni-lock"></i>
+        admin access
+      </router-link>
+      <!---label for="menu" class="hamburger"></label-->
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeaderVue'
+}
+</script>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="hero">
+    <div class="row m-0 align-items-center justify-content-between">
+      <div class="col-md-6 p-0">
+        <div class="hero__text">
+          <h1 class="hero__heading">
+            Find over 300 <span>CoD: Mobile</span> Teams
+          </h1>
+        </div>
+      </div>
+      <div class="col-md-3"></div>
+      <div class="col-md-3">
+        <img src="@/assets/img/placeholder.png" class="hero__img" alt="COD Ops" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AppHeroVue'
+}
+</script>
